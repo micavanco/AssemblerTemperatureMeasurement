234ac6600247d4df4dcbeda2701d9fe1dac20d1e
Michał O <micavanco@gmai.com>
2018-07-09 18:49:55 +0200

    Added button handling

--- AssemblerTemperatureMeasurement/main.asm
+++ AssemblerTemperatureMeasurement/main.asm
@@ -17,11 +17,10 @@ start:
 .DEF BLUE_LED=R22
 .DEF counter=R17
 
-
 cli					;disable interrupts 
 
 ldi counter, 200
-ldi RED_LED, 2			
+ldi RED_LED, 2		
 
 lds R16, TCCR2A		;load address of TCCR2A to R16
 ORI R16, (1<<COM2A1)|(1<<COM2A0)|(1<<WGM20)|(1<<WGM21) ; load data to R16 (logical OR operation), set OC2A on Compare Match, clear OC2A at BOTTOM (inverting mode) fastPWM
@@ -45,29 +44,34 @@ lds R16, TIMSK0		;load address of TIMSK0 to R16
 ORI R16, (1<<OCIE0A);load to R16 data (logical OR operation), set interrupt on compare enable
 sts TIMSK0, R16		;store direct to data space from R16
 
+sbi DDRC, DDC2		;set Data Direct Register as a input
 sbi DDRC, DDC1		;set Data Direct Register as a input
 sbi DDRD, DDD7
+sbi DDRD, (0<<DDD6) ;set Data Direct Register as a output (connected button)
 
 ldi R16, (1<<PINC1)|(1<<PINC2) ;set Port as a output
 out PORTC, R16 
-ldi R16, (0<<PIND7)
+ldi R16, (0<<PIND7)|(1<<PIND6) ;pull-up internal resistor in PIND6
 out PORTD, R16 
 sei					;enable interrupts
 
 ;*************************************************
 main:				;main loop
+sbic PIND, PIND6	;wait for pushed button (if PIND6 is clear, skip next instruction)
+rjmp main
 nop
-inc R18				;increment value 
-cpi	R18, 252		;if R15 equals 254 do next command
+inc RED_LED			;increment value
+cpi	RED_LED, 252	;if R18 equals or higher 252 do next command
 brsh toone
-lds R16, OCR2A		;load address of OCR2A to R16
-mov R16, RED_LED	;load value to R16
-sts OCR2A, R16		;store direct to data space from R16
+lds R19, OCR2A		;load address of OCR2A to R16
+mov R19, RED_LED	;load value to R16
+sts OCR2A, R19		;store direct to data space from R16
 nop 
 rjmp delay
+rjmp main
 toone: 
 ldi RED_LED, 1
-rjmp main
+rjmp delay
 ;*************************************************		
 
 Tim0_compA:
@@ -90,7 +94,7 @@ reti				;return from interrupt
 
 delay:
     ldi  r16, 10
-    ldi  r24, 207
+    ldi  r24, 220
 L1: dec  r16
     brne L1
     dec  r24
